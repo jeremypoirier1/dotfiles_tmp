3d1b40aff109e5be5891aa3af03a9000dc6324fc
Jeremy <6302610@cstjean.qc.ca>
2025-08-28 12:09:03 -0400

    ajout fichier

---
+++ install.sh
@@ -0,0 +1,7 @@
+echo "# dotfiles_tmp" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:jeremypoirier1/dotfiles_tmp.git
+git push -u origin main
